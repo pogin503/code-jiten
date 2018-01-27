b5d5fad75fe01777ece33a56f068f76e2808f0cc
pogin503 <pogin503@gmail.com>
2018-01-27 16:41:45 +0900

    style: Move mock.php

--- mock/mock.php
+++ mock/mock.php
@@ -29,8 +29,8 @@
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="#">Features</a>
-          <a class="nav-item nav-link" href="#">Pricing</a>
+          <a class="nav-item nav-link" href="#">Mock</a>
+          <a class="nav-item nav-link" href="#">Register</a>
         </div>
       </div>
     </nav>
