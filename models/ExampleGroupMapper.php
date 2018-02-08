4082596a4e4e98a60ba6ebfdce091b15a42939ed
pogin503 <pogin503@gmail.com>
2018-02-08 23:52:15 +0900

    style: untabify

--- models/ExampleGroupMapper.php
+++ models/ExampleGroupMapper.php
@@ -11,11 +11,11 @@ class ExampleGroupMapper extends Eloquent {
     }
 
     public function getChild($group_cd){
-//         $data = DB::select("
-// SELECT group_cd, group_name, group_level, \"desc\", disp_flag
-// FROM t_example_group
-// where group_cd = :group_cd;", ['group_cd' => $group_cd]);
-// 	return $data;
+        //         $data = DB::select("
+        // SELECT group_cd, group_name, group_level, \"desc\", disp_flag
+        // FROM t_example_group
+        // where group_cd = :group_cd;", ['group_cd' => $group_cd]);
+        // 	return $data;
     }
 
     public function getParent(){
@@ -34,26 +34,30 @@ class ExampleGroupMapper extends Eloquent {
     }
 
     public function deleteGroup($group_cds) {
+        DB::table('t_example_relation')
+            ->whereIn('group_descendant', $group_cds)
+            ->delete();
+
         DB::table('t_example_group')
             ->whereIn('group_cd', $group_cds)
             ->delete();
     }
     public function insertGroup($group_name, $group_level,
-				$desc, $disp_flag, $parent_group_cd) {
-	DB::beginTransaction();
-	$insert_stmt = DB::getPdo()->prepare('
+                                $desc, $disp_flag, $parent_group_cd) {
+        DB::beginTransaction();
+        $insert_stmt = DB::getPdo()->prepare('
 INSERT INTO t_example_group (group_name, group_level, "desc", disp_flag)
 VALUES (:group_name, :group_level, :desc, :disp_flag) RETURNING group_cd;');
-	$insert_stmt->execute(
-		   [
-		       ':group_name' => $group_name,
-		       ':group_level' => $group_level,
-		       ':desc' => $desc,
-		       ':disp_flag' => $disp_flag,
-		   ]);
-	$inserted_group_cd = $insert_stmt->fetchALL(PDO::FETCH_COLUMN);
+        $insert_stmt->execute(
+            [
+                ':group_name' => $group_name,
+                ':group_level' => $group_level,
+                ':desc' => $desc,
+                ':disp_flag' => $disp_flag,
+            ]);
+        $inserted_group_cd = $insert_stmt->fetchALL(PDO::FETCH_COLUMN);
 
-	$insert_stmt2 = DB::getPdo()->prepare('
+        $insert_stmt2 = DB::getPdo()->prepare('
 INSERT INTO t_example_relation (group_ancestor, group_descendant)
   (SELECT t.group_ancestor, cast(:group_cd1 as integer)
   FROM t_example_relation AS t
@@ -61,11 +65,11 @@ INSERT INTO t_example_relation (group_ancestor, group_descendant)
   UNION ALL
   (SELECT cast(:group_cd2 as integer), cast(:group_cd3 as integer));');
 
-	$insert_stmt2->bindValue(':group_cd1', $inserted_group_cd[0], PDO::PARAM_INT);
-	$insert_stmt2->bindValue(':parent_group_cd', $parent_group_cd, PDO::PARAM_INT);
-	$insert_stmt2->bindValue(':group_cd2', $inserted_group_cd[0], PDO::PARAM_INT);
-	$insert_stmt2->bindValue(':group_cd3', $inserted_group_cd[0], PDO::PARAM_INT);
-	$insert_stmt2->execute();
-	DB::commit();
+        $insert_stmt2->bindValue(':group_cd1', $inserted_group_cd[0], PDO::PARAM_INT);
+        $insert_stmt2->bindValue(':parent_group_cd', $parent_group_cd, PDO::PARAM_INT);
+        $insert_stmt2->bindValue(':group_cd2', $inserted_group_cd[0], PDO::PARAM_INT);
+        $insert_stmt2->bindValue(':group_cd3', $inserted_group_cd[0], PDO::PARAM_INT);
+        $insert_stmt2->execute();
+        DB::commit();
     }
 }
