63fd8310d7496af7c91409d6f5f39577770f918e
pogin503 <pogin503@gmail.com>
2018-02-23 00:34:08 +0900

    refactor: fix order

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 define('DB_DRIVER', 'pgsql');
+define('DB_HOST', 'localhost');
 define('DB_DATABASE', 'postgres');
+define('DB_PORT', '5432');
+define('DB_CHARSET', 'UTF8');
 define('DB_USERNAME', 'postgres');
 define('DB_PASSWD', 'postgres');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'UTF8');
-define('DB_PORT', '5432');
 define('PDO_DSN', DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";port=" . DB_PORT. "charset=" . DB_CHARSET);
 
 use Illuminate\Database\Capsule\Manager as Capsule;
