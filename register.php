58da8ef3aaaa36d9b72dfb1211a10cbf57a4f32a
pogin503 <pogin503@gmail.com>
2018-03-02 00:39:08 +0900

    refactor: Remove debug setting

--- register.php
+++ register.php
@@ -2,12 +2,6 @@
 <html>
   <head>
     <?php
-    ini_set('display_startup_errors',1);
-    ini_set('display_errors',1);
-    error_reporting(-1);
-    error_reporting(E_ALL);
-    ?>
-    <?php
     require_once './vendor/autoload.php';
     require_once './models/Example.php';
     require_once './models/ExampleGroup.php';
