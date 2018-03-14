4b7c29e800d1208cda1f376bfb47533c9157d88e
pogin503 <pogin503@gmail.com>
2018-03-14 22:42:01 +0900

    style: Reformat

--- register.php
+++ register.php
@@ -1,145 +1,143 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php
-    require_once './vendor/autoload.php';
-    require_once './models/Example.php';
-    require_once './models/ExampleGroup.php';
-    require_once './models/ExampleGroupMapper.php';
-    require_once './models/Language.php';
-    require_once './src/functions.php';
-    require_once './config/database.php';
+<?php
+require_once './vendor/autoload.php';
+require_once './models/Example.php';
+require_once './models/ExampleGroup.php';
+require_once './models/ExampleGroupMapper.php';
+require_once './models/Language.php';
+require_once './src/functions.php';
+require_once './config/database.php';
 
-    $loader = new Twig_Loader_Filesystem('views');
-    $twig = new Twig_Environment($loader, array(
-      //'cache' => './compilation_cache',
-      'debug' => true,
-    ));
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader, array(
+    //'cache' => './compilation_cache',
+    'debug' => true,
+));
 
-    $twig->addExtension(new Twig_Extension_Debug());
-    $template = $twig->load('header.html.twig');
-    echo $template->render();
+$twig->addExtension(new Twig_Extension_Debug());
+echo $twig->load('header.html.twig')->render();
 
-    $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWD);
-    $group_cd = isset($_GET['group_cd']) ? $_GET['group_cd'] : null;
-    $parent_id = isset($_GET['parent_id']) ? $_GET['parent_id'] : null;
+$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWD);
+$group_cd = isset($_GET['group_cd']) ? $_GET['group_cd'] : null;
+$parent_id = isset($_GET['parent_id']) ? $_GET['parent_id'] : null;
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      if(isset($_GET['group_cd'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(isset($_GET['group_cd'])) {
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $insert_stmt = $db->prepare("INSERT INTO t_example (\"language\", \"example\", \"group_cd\") VALUES
 (:language, :example, :group_cd)");
         $update_stmt = $db->prepare("UPDATE t_example SET language = :language, example = :example
 WHERE example_id = :example_id;");
         if (!empty($_POST['items'])) {
-          foreach ($_POST['items'] as $row) {
-            if ($row['insert_flag'] == "true") {
-              $insert_stmt->bindParam(':language',$row['example']['language']);
-              $insert_stmt->bindParam(':example', $row['example']['example']);
-              $insert_stmt->bindParam(':group_cd',intval($row['group_cd']));
-              $insert_stmt->execute();
-            } else {
-              $update_stmt->bindParam(':language',$row['example']['language']);
-              $update_stmt->bindParam(':example', $row['example']['example']);
-              $update_stmt->bindParam(':example_id',intval($row['example']['example_id']));
-              $update_stmt->execute();
+            foreach ($_POST['items'] as $row) {
+                if ($row['insert_flag'] == "true") {
+                    $insert_stmt->bindParam(':language',$row['example']['language']);
+                    $insert_stmt->bindParam(':example', $row['example']['example']);
+                    $insert_stmt->bindParam(':group_cd',intval($row['group_cd']));
+                    $insert_stmt->execute();
+                } else {
+                    $update_stmt->bindParam(':language',$row['example']['language']);
+                    $update_stmt->bindParam(':example', $row['example']['example']);
+                    $update_stmt->bindParam(':example_id',intval($row['example']['example_id']));
+                    $update_stmt->execute();
+                }
             }
-          }
         }
         if (!empty($_POST['delete_target'])) {
-          $delete_stmt = $db->prepare('DELETE FROM t_example WHERE example_id = :example_id;');
-          foreach($_POST['delete_target'] as $example_id){
-            $delete_stmt->bindParam(':example_id', intval($example_id));
-            $delete_stmt->execute();
-          }
+            $delete_stmt = $db->prepare('DELETE FROM t_example WHERE example_id = :example_id;');
+            foreach($_POST['delete_target'] as $example_id){
+                $delete_stmt->bindParam(':example_id', intval($example_id));
+                $delete_stmt->execute();
+            }
         }
-
-      }
     }
-    $_POST = array();
-    $json = '';
-    $examples = '';
-    $disp_group = '';
-    $group_name = null;
-    $group_data_json = '';
+}
+$_POST = array();
+$json = '';
+$examples = '';
+$disp_group = '';
+$group_name = null;
+$group_data_json = '';
 
-    if(isset($group_cd)) {
+if(isset($group_cd)) {
 
-      // example data
-      $examples_stmt = $db->prepare("SELECT example_id, language, example, group_cd, group_name FROM v_example_desc WHERE group_cd = :group_cd;");
-      $examples_stmt->bindParam(':group_cd', intval($group_cd));
-      $examples_stmt->execute();
-      $example_records = $examples_stmt->fetchALL(PDO::FETCH_ASSOC);
+    // example data
+    $examples_stmt = $db->prepare("SELECT example_id, language, example, group_cd, group_name FROM v_example_desc WHERE group_cd = :group_cd;");
+    $examples_stmt->bindParam(':group_cd', intval($group_cd));
+    $examples_stmt->execute();
+    $example_records = $examples_stmt->fetchALL(PDO::FETCH_ASSOC);
 
-      if (!empty($example_records)) {
+    if (!empty($example_records)) {
         $examples = array_map(function ($record) {
-          return new Example($record);
+            return new Example($record);
         }, $example_records);
 
         $group_name = $example_records[0]['group_name'];
-      } else {
+    } else {
         $group_name_stmt = $db->prepare("SELECT group_name FROM t_example_group WHERE group_cd = :group_cd");
         $group_name_stmt->bindParam(':group_cd', $group_cd);
         $group_name_stmt->execute();
         $group_name_row = $group_name_stmt->fetch();
         $group_name = $group_name_row['group_name'];
-      }
+    }
 
-      $languages = Language::getLanguage()->toArray();
-      if(empty($examples)) {
+    $languages = Language::getLanguage()->toArray();
+    if(empty($examples)) {
         $json = json_encode([
             'items' => [],
             'languages' => $languages
         ]);
-      } else {
+    } else {
 
         $items = array_map(function($i, $idx) {
             return [
-              'example' => $i->toArray(),
+                'example' => $i->toArray(),
                 'insert_flag' => false,
                 'update_flag' => false,
                 'row_num' => $idx
             ];
-          }, $examples, range(1, count($examples)));
+        }, $examples, range(1, count($examples)));
 
         $json = json_encode([
-          'items' => $items,
-          'group_cd' => $group_cd,
-          'languages' => $languages]);
-      }
-
-      // group data
-      $group_data = array_map(function($i) {
-        return new ExampleGroup($i);
-      }, ExampleGroupMapper::fetchParents($group_cd));
-
-      if (empty($group_data)) {
-        $group_data_json = json_encode(['group_names' => []]);
-      } else {
-        $group_data_json = json_encode(['group_names' =>
-          array_map(function($i) {
-            return [
-              'group_cd' => $i->group_cd,
-                'group_name' => $i->group_name
-            ];
-          }, $group_data),
-        ]);
-      }
-
-    } else {
-      if (empty($parent_id)) {
-        $disp_group = json_encode([
-          'items' => ExampleGroupMapper::fetchLeaf()->toArray(),
-            'seen' => true
-        ]);
-      } else {
-        $disp_group = json_encode([
-          'items' => ExampleGroupMapper::fetchChilds(intval($parent_id)),
-            'seen' => true
-        ]);
-      }
-
+            'items' => $items,
+            'group_cd' => $group_cd,
+            'languages' => $languages]);
     }
+
+    // group data
+    $group_data = array_map(function($i) {
+        return new ExampleGroup($i);
+    }, ExampleGroupMapper::fetchParents($group_cd));
+
+    if (empty($group_data)) {
+        $group_data_json = json_encode(['group_names' => []]);
+    } else {
+        $group_data_json = json_encode(
+            ['group_names' =>
+             array_map(function($i) {
+                 return [
+                     'group_cd' => $i->group_cd,
+                     'group_name' => $i->group_name
+                 ];
+             }, $group_data),
+        ]);
+    }
+
+} else {
+    if (empty($parent_id)) {
+        $disp_group = json_encode([
+            'items' => ExampleGroupMapper::fetchLeaf()->toArray(),
+            'seen' => true
+        ]);
+    } else {
+        $disp_group = json_encode([
+            'items' => ExampleGroupMapper::fetchChilds(intval($parent_id)),
+            'seen' => true
+        ]);
+    }
+}
 
     ?>
   </head>
