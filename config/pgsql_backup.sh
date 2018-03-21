b42dfd195d0822b25506629d8f66aacc95acb95c
pogin503 <pogin503@gmail.com>
2018-03-22 01:07:18 +0900

    chore: Update pgsql_backup.sh

--- config/pgsql_backup.sh
+++ config/pgsql_backup.sh
@@ -6,6 +6,14 @@ set -eu
 # shellcheck source=env.sh
 . env.sh
 DATE_DIR=$(date '+%Y-%m-%d')
+
 mkdir -p "$OUTPUT_DIR/$DATE_DIR"
-pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --schema-only "$DBNAME" --if-exists --clean > "$OUTPUT_DIR/${DATE_DIR}/${SCHEMA}_$(date '+%Y%m%d_%H%M')"
-pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --data-only --disable-triggers "$DBNAME" > "$OUTPUT_DIR/${DATE_DIR}/${DATA}_$(date '+%Y%m%d_%H%M')"
+
+IFS=' '
+TABLES="t_example t_example_group t_example_relation t_language t_language_template v_example_desc"
+for TABLE in $TABLES; do
+	pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --schema-only "$DBNAME" -t "$TABLE" --if-exists --clean \
+		> "$OUTPUT_DIR/${DATE_DIR}/${SCHEMA}_$(date '+%Y%m%d_%H%M')_${TABLE}.sql"
+	pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --data-only --disable-triggers "$DBNAME" -t "$TABLE" \
+		> "$OUTPUT_DIR/${DATE_DIR}/${DATA}_$(date '+%Y%m%d_%H%M')_${TABLE}.sql"
+done
