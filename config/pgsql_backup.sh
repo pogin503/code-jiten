d174185bb2a9d32102feb4b2c0c5dd281d8b8ded
pogin503 <pogin503@gmail.com>
2018-03-27 18:14:27 +0900

    chore: Collect table backups in one file

--- config/pgsql_backup.sh
+++ config/pgsql_backup.sh
@@ -17,3 +17,13 @@ for TABLE in $TABLES; do
 	pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --data-only --disable-triggers "$DBNAME" -t "$TABLE" \
 		> "$OUTPUT_DIR/${DATE_DIR}/${DATA}_$(date '+%Y%m%d_%H%M')_${TABLE}.sql"
 done
+
+TABLES_WITH_OPS=$(IFS=' '; for x in $TABLES; do printf " -t %s" "$x"; done)
+
+# shellcheck disable=SC2086
+pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --schema-only "$DBNAME" --if-exists --clean ${TABLES_WITH_OPS} \
+		    > "$OUTPUT_DIR/${DATE_DIR}/${SCHEMA}_$(date '+%Y%m%d_%H%M').sql"
+
+# shellcheck disable=SC2086
+pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --data-only --disable-triggers "$DBNAME" ${TABLES_WITH_OPS} \
+		    > "$OUTPUT_DIR/${DATE_DIR}/${DATA}_$(date '+%Y%m%d_%H%M').sql"
