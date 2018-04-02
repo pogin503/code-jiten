8449088aa2bc3de05e0fec6ac4987b4cff17e8c0
pogin503 <pogin503@gmail.com>
2018-04-03 00:09:08 +0900

    refactor: Remove unnecessary setting

--- config/pgsql_backup.sh
+++ config/pgsql_backup.sh
@@ -9,7 +9,6 @@ DATE_DIR=$(date '+%Y-%m-%d-%H%M')
 
 mkdir -p "$BACKUP_DIR/$DATE_DIR"
 
-IFS=' '
 TABLES="t_example t_example_group t_example_relation t_language t_language_template v_example_desc"
 for TABLE in $TABLES; do
 	pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --schema-only "$DBNAME" -t "$TABLE" --if-exists --clean \
@@ -18,7 +17,7 @@ for TABLE in $TABLES; do
 		> "$BACKUP_DIR/${DATE_DIR}/${DATA}_${TABLE}.sql"
 done
 
-TABLES_WITH_OPS=$(IFS=' '; for x in $TABLES; do printf " -t %s" "$x"; done)
+TABLES_WITH_OPS=$(for x in $TABLES; do printf " -t %s" "$x"; done)
 
 # shellcheck disable=SC2086
 pg_dump --username="$USERNAME" -h "$HOST" -p "$PORT" --schema-only "$DBNAME" --if-exists --clean ${TABLES_WITH_OPS} \
