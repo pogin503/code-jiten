e251baf3bd520e8cdf69ea4ff751cc5c37160a65
pogin503 <pogin503@gmail.com>
2018-04-02 23:59:32 +0900

    chore: Update pgsql_restore.sh

--- config/pgsql_restore.sh
+++ config/pgsql_restore.sh
@@ -6,10 +6,11 @@ set -eu
 # shellcheck source=env.sh
 . env.sh
 
-mkdir -p $RESTORE_DIR
-echo "DROP DATABASE IF EXISTS $DBNAME;" | psql --username="$USERNAME" -h "$HOST" -p "$PORT" template1
-# echo "create database $DBNAME with encoding = 'UTF8' LC_COLLATE = 'ja_JP.UTF-8' LC_CTYPE = 'ja_JP.UTF-8';" | psql --username="$USERNAME" -h "$HOST" template1
-echo "CREATE DATABASE $DBNAME with encoding = 'UTF8';" | psql --username="$USERNAME" -h "$HOST" -p "$PORT" template1
+# disconnect session
+echo "SELECT pg_terminate_backend(pid) \
+   FROM pg_stat_activity \
+  WHERE datname = '$DBNAME' \
+    AND pid <> pg_backend_pid();" | psql --username="$USERNAME" -h "$HOST" -p "$PORT" template1
 
 psql --username="$USERNAME" -h "$HOST" -p "$PORT" "$DBNAME" < "${RESTORE_DIR}/${SCHEMA}.sql"
 psql --username="$USERNAME" -h "$HOST" -p "$PORT" "$DBNAME" < "${RESTORE_DIR}/${DATA}.sql"
