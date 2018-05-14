b289556ec56d706d521fb41b6665907900e1c73a
pogin503 <pogin503@gmail.com>
2018-05-15 00:43:55 +0900

    style: Reformat

--- config/database.php
+++ config/database.php
@@ -6,23 +6,30 @@ define('DB_PORT', '5432');
 define('DB_CHARSET', 'UTF8');
 define('DB_USERNAME', 'postgres');
 define('DB_PASSWD', 'postgres');
-define('PDO_DSN', DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";port=" . DB_PORT. "charset=" . DB_CHARSET);
+define(
+    'PDO_DSN', DB_DRIVER . ":host=" . DB_HOST
+    . ";dbname=" . DB_DATABASE
+    . ";port=" . DB_PORT
+    . "charset=" . DB_CHARSET
+);
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
 
-$capsule->addConnection([
-    'driver'    => DB_DRIVER,
-    'host'      => DB_HOST,
-    'database'  => DB_DATABASE,
-    'port'      => DB_PORT,
-    'username'  => DB_USERNAME,
-    'password'  => DB_PASSWD,
-    'charset'   => DB_CHARSET,
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
+$capsule->addConnection(
+    [
+        'driver'    => DB_DRIVER,
+        'host'      => DB_HOST,
+        'database'  => DB_DATABASE,
+        'port'      => DB_PORT,
+        'username'  => DB_USERNAME,
+        'password'  => DB_PASSWD,
+        'charset'   => DB_CHARSET,
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]
+);
 
 // Set the event dispatcher used by Eloquent models... (optional)
 use Illuminate\Events\Dispatcher;
